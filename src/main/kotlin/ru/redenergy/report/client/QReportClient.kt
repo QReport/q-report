469aa506562af87055eeb81a33e12da292a7b0a9
RedEnergy <forwot163@gmail.com>
2015-12-01 21:33:56 +0400

    QReport Client: Added keys and packets registration"

--- src/main/kotlin/ru/redenergy/report/client/QReportClient.kt
+++ src/main/kotlin/ru/redenergy/report/client/QReportClient.kt
@@ -1,14 +1,40 @@
 package ru.redenergy.report.client
 
+import com.rabbit.gui.GuiFoundation
 import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.event.FMLInitializationEvent
 import cpw.mods.fml.common.event.FMLPostInitializationEvent
+import net.minecraft.client.settings.KeyBinding
+import org.lwjgl.input.Keyboard
+import ru.redenergy.report.client.keyboard.KeyboardController
+import ru.redenergy.report.client.ui.ReportShow
+import ru.redenergy.report.common.network.NetworkHandler
+import ru.redenergy.report.common.network.packet.ReportPacket
 
 @Mod(modid = "qreport-client", name = "QReport - Q&A communication tool for players and admins", modLanguageAdapter = "ru.redenergy.report.kotlin.KotlinAdapter")
 object QReportClient {
 
     @Mod.EventHandler
     public fun postInit(event: FMLPostInitializationEvent){
+        registerKeys()
+        NetworkHandler.instance.initialise()
         println("QReport Loaded")
+    }
+
+    @Mod.EventHandler
+    public fun init(event: FMLInitializationEvent){
+        registerPackets()
+    }
+
+    private fun registerPackets(){
+        NetworkHandler.instance.registerPacket(ReportPacket::class)
+    }
+
+    private fun registerKeys(){
+        NetworkHandler.instance.postInitialize()
+        var keyboardController = KeyboardController()
+        keyboardController.register(Keyboard.KEY_R, "Opens ticket form") { if(Keyboard.isKeyDown(Keyboard.KEY_TAB)) GuiFoundation.display(ReportShow()) }
+        keyboardController.submit()
     }
 }
 
