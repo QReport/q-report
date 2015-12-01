4785a49690b4f312736ccec39ab2193d8827cb6a
RedEnergy <forwot163@gmail.com>
2015-12-01 09:47:20 +0400

    ReportManagerTest: Added addReport and getReports tests

--- src/test/kotlin/ru/redenergy/report/server/backend/ReportManagerTest.kt
+++ src/test/kotlin/ru/redenergy/report/server/backend/ReportManagerTest.kt
@@ -1,11 +1,32 @@
 package ru.redenergy.report.server.backend
 
+import com.j256.ormlite.jdbc.JdbcConnectionSource
+import com.j256.ormlite.table.TableUtils
+import org.junit.Before
+import org.junit.Test
+import ru.redenergy.report.server.backend.entity.Report
+import java.io.File
 import org.junit.Assert.*
+
+val path = "jdbc:sqlite:${File("").absolutePath}/reports-test.sqlite"
 
 class ReportManagerTest {
 
-    @org.junit.Test
-    fun testInitialize() {
+    val reportManager = ReportManager(JdbcConnectionSource(path))
 
+    @Before
+    fun testInitialize() {
+        reportManager.initialize()
     }
+
+    @Test
+    fun testAddReport(){
+        reportManager.addReport(Report(sender = "MagicPlayer", text = "Text"))
+    }
+
+    @Test
+    fun testGetReports(){
+        reportManager.getReports().forEach { println(it) }
+    }
+
 }
