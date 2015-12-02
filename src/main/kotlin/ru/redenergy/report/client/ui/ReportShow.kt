59905fad5bd6cf05b25586cbabad035e3889dd89
RedEnergy <forwot163@gmail.com>
2015-12-02 11:38:18 +0400

    ReportShow: Added dropdown with ticket reasons

--- src/main/kotlin/ru/redenergy/report/client/ui/ReportShow.kt
+++ src/main/kotlin/ru/redenergy/report/client/ui/ReportShow.kt
@@ -2,11 +2,13 @@ package ru.redenergy.report.client.ui
 
 import com.rabbit.gui.background.DefaultBackground
 import com.rabbit.gui.component.control.Button
+import com.rabbit.gui.component.control.DropDown
 import com.rabbit.gui.component.control.MultiTextbox
 import com.rabbit.gui.component.control.TextBox
 import com.rabbit.gui.show.Show
+import ru.redenergy.report.common.TicketReason
 import ru.redenergy.report.common.network.NetworkHandler
-import ru.redenergy.report.common.network.packet.ReportPacket
+import ru.redenergy.report.common.network.packet.TicketPacket
 
 class ReportShow : Show() {
 
@@ -19,15 +21,20 @@ class ReportShow : Show() {
         super.setup()
         registerComponent(MultiTextbox(this.width / 3, this.height / 3, this.width / 3, this.height / 3)
                 .setId("text_box"))
-        registerComponent(Button(this.width / 3, this.height / 3 * 2 + 2, this.width / 3, 20, "Send")
+        registerComponent(DropDown<TicketReason>(this.width / 3, this.height / 3 * 2 + 5, this.width / 3, "Ticket reason:")
+                .addAndSetDefault(TicketReason.OTHER)
+                .addAll(*TicketReason.values.filter { !it.equals(TicketReason.OTHER) }.toTypedArray())
+                .setId("reason_dropdown"))
+        registerComponent(Button(this.width / 3, this.height / 3 * 2 + 22, this.width / 3, 20, "Send")
                 .setClickListener { send() })
-        registerComponent(Button(this.width / 3, this.height / 3 * 2 + 24, this.width / 3, 20, "Close")
+        registerComponent(Button(this.width / 3, this.height / 3 * 2 + 44, this.width / 3, 20, "Close")
                 .setClickListener { this.getStage().displayPrevious() })
     }
 
     private fun send(){
         var text = (findComponentById<TextBox>("text_box") as MultiTextbox).text
-        NetworkHandler.instance.sendToServer(ReportPacket(text))
+        var reason = (findComponentById<DropDown<TicketReason>>("reason_dropdown") as DropDown<TicketReason>).selectedElement.value
+        NetworkHandler.instance.sendToServer(TicketPacket(text, reason))
         this.getStage().displayPrevious()
     }
 
