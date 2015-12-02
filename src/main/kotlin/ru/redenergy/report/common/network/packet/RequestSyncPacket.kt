88d3459bfa9713c5e076676c251d27c32d429431
RedEnergy <forwot163@gmail.com>
2015-12-02 13:10:58 +0400

    Packets: Added packet for sync request and syncing packet itself

---
+++ src/main/kotlin/ru/redenergy/report/common/network/packet/RequestSyncPacket.kt
@@ -0,0 +1,24 @@
+package ru.redenergy.report.common.network.packet
+
+import io.netty.buffer.ByteBuf
+import io.netty.channel.ChannelHandlerContext
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.entity.player.EntityPlayerMP
+import ru.redenergy.report.client.QReportServer
+import ru.redenergy.report.common.network.AbstractPacket
+
+/**
+ * Sent from client ot server when user opens ui with previous requests
+ */
+class RequestSyncPacket() : AbstractPacket{
+
+    override fun encodeInto(ctx: ChannelHandlerContext, buf: ByteBuf) {}
+
+    override fun decodeInto(ctx: ChannelHandlerContext, buf: ByteBuf) {}
+
+    override fun handleClient(player: EntityPlayer) {}
+
+    override fun handleServer(player: EntityPlayer) {
+        QReportServer.ticketManager.handleSyncRequest(player as EntityPlayerMP)
+    }
+}
---
+++ src/main/kotlin/ru/redenergy/report/common/network/packet/SyncTickets.kt
@@ -0,0 +1,34 @@
+package ru.redenergy.report.common.network.packet
+
+import com.google.gson.Gson
+import com.rabbit.gui.GuiFoundation
+import cpw.mods.fml.common.network.ByteBufUtils
+import io.netty.buffer.ByteBuf
+import io.netty.channel.ChannelHandlerContext
+import net.minecraft.entity.player.EntityPlayer
+import org.apache.commons.lang3.reflect.FieldUtils
+import ru.redenergy.report.client.QReportClient
+import ru.redenergy.report.common.entity.Ticket
+import ru.redenergy.report.common.network.AbstractPacket
+
+class SyncTickets(var tickets: MutableList<Ticket>) : AbstractPacket{
+
+    constructor() : this(arrayListOf()) {}
+
+    override fun encodeInto(ctx: ChannelHandlerContext, buf: ByteBuf) {
+        ByteBufUtils.writeUTF8String(buf, Gson().toJson(tickets))
+    }
+
+    override fun decodeInto(ctx: ChannelHandlerContext, buf: ByteBuf) {
+        this.tickets = Gson().fromJson(ByteBufUtils.readUTF8String(buf),
+                FieldUtils.getField(SyncTickets::class.java, "tickets", true).genericType)
+    }
+
+    override fun handleClient(player: EntityPlayer) {
+        QReportClient.syncedTickets.clear()
+        QReportClient.syncedTickets.addAll(tickets)
+        GuiFoundation.getCurrentStage()?.reinitShow()
+    }
+
+    override fun handleServer(player: EntityPlayer) {}
+}
