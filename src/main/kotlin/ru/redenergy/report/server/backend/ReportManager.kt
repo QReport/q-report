d7a94bf3e5416082431e2fdc25234a57a73f106e
RedEnergy <forwot163@gmail.com>
2015-12-03 12:53:55 +0400

    ReportManager: Added json persister registration, updated #newTicket function, updated ticket configuration

--- src/main/kotlin/ru/redenergy/report/server/backend/ReportManager.kt
+++ src/main/kotlin/ru/redenergy/report/server/backend/ReportManager.kt
@@ -2,23 +2,32 @@ package ru.redenergy.report.server.backend
 
 import com.j256.ormlite.dao.Dao
 import com.j256.ormlite.dao.DaoManager
+import com.j256.ormlite.field.DataPersisterManager
 import com.j256.ormlite.field.DatabaseFieldConfig
 import com.j256.ormlite.support.ConnectionSource
 import com.j256.ormlite.table.DatabaseTableConfig
 import com.j256.ormlite.table.TableUtils
 import net.minecraft.entity.player.EntityPlayerMP
+import org.apache.commons.lang3.reflect.FieldUtils
 import ru.redenergy.report.common.TicketReason
 import ru.redenergy.report.common.entity.Ticket
+import ru.redenergy.report.common.entity.TicketMessage
 import ru.redenergy.report.common.network.NetworkHandler
 import ru.redenergy.report.common.network.packet.SyncTickets
+import ru.redenergy.report.server.orm.JsonPersister
 import java.util.*
+import kotlin.reflect.declaredFunctions
 
 class ReportManager(val connectionSource: ConnectionSource) {
+
+    init{
+        DataPersisterManager.registerDataPersisters(JsonPersister.getSingleton())
+    }
 
     val ticketConfig = configureTicketEntity()
     var ticketDao = DaoManager.createDao<Dao<Ticket, UUID>, Ticket>(connectionSource, ticketConfig)
 
-    public fun initialize(){
+    public fun initialize() {
         TableUtils.createTableIfNotExists(connectionSource, Ticket::class.java)
     }
 
@@ -30,17 +39,16 @@ class ReportManager(val connectionSource: ConnectionSource) {
 
     public fun handleNewTicket(text: String, reason: TicketReason, player: EntityPlayerMP) = newTicket(text, reason, player.commandSenderName)
 
-    public fun newTicket(text: String, reason: TicketReason, sender: String) = addTicket(Ticket(sender = sender, text = text, reason = reason))
+    public fun newTicket(text: String, reason: TicketReason, sender: String) = addTicket(Ticket(sender = sender, reason = reason, messages = arrayListOf(TicketMessage(sender, text))))
 
     public fun handleSyncRequest(player: EntityPlayerMP) = NetworkHandler.instance.sendTo(SyncTickets(getTickets()), player)
 
-    private fun configureTicketEntity() : DatabaseTableConfig<Ticket> {
+    private fun configureTicketEntity(): DatabaseTableConfig<Ticket> {
         return DatabaseTableConfig(Ticket::class.java, arrayListOf<DatabaseFieldConfig>().apply {
             add(DatabaseFieldConfig("uid").apply { isId = true })
             add(DatabaseFieldConfig("sender"))
-            add(DatabaseFieldConfig("text"))
             add(DatabaseFieldConfig("reason"))
-            add(DatabaseFieldConfig("timestamp"))
+            add(DatabaseFieldConfig("messages").apply { persisterClass = JsonPersister::class.java })
         }).apply { tableName = "tickets" }
     }
 }
