a009689ce0cd4ec2a7d9cb9d543eccbb32358995
RedEnergy <forwot163@gmail.com>
2015-12-06 15:34:40 +0400

    ReportShow: Localized strings

--- src/main/kotlin/ru/redenergy/report/client/ui/ReportShow.kt
+++ src/main/kotlin/ru/redenergy/report/client/ui/ReportShow.kt
@@ -7,6 +7,7 @@ import com.rabbit.gui.component.control.MultiTextbox
 import com.rabbit.gui.component.control.TextBox
 import com.rabbit.gui.component.display.TextLabel
 import com.rabbit.gui.show.Show
+import net.minecraft.client.resources.I18n
 import ru.redenergy.report.common.TicketReason
 import ru.redenergy.report.common.network.NetworkHandler
 import ru.redenergy.report.common.network.packet.TicketPacket
@@ -20,17 +21,17 @@ class ReportShow : Show() {
 
     public override fun setup(){
         super.setup()
-        registerComponent(TextLabel(this.width / 3, this.height / 4 - 15, this.width / 3, "Describe your problem:"))
+        registerComponent(TextLabel(this.width / 3, this.height / 4 - 15, this.width / 3, I18n.format("show.report.problem")))
         registerComponent(MultiTextbox(this.width / 3, this.height / 4, this.width / 3, this.height / 3)
                 .setId("text_box"))
-        registerComponent(TextLabel(this.width / 3, this.height / 4 + this.height / 3 + 20, this.width /3, "Related category:"))
+        registerComponent(TextLabel(this.width / 3, this.height / 4 + this.height / 3 + 20, this.width /3, I18n.format("show.report.category")))
         registerComponent(DropDown<TicketReason>(this.width / 3 + 2, this.height / 4 + this.height / 3 + 35, this.width / 3 - 4)
                 .addAndSetDefault(TicketReason.OTHER)
                 .addAll(*TicketReason.values.filter { !it.equals(TicketReason.OTHER) }.toTypedArray())
                 .setId("reason_dropdown"))
-        registerComponent(Button(this.width / 3, this.height / 3 + this.height / 4 + 60, this.width / 3 / 2 - 2, 20, "Send")
+        registerComponent(Button(this.width / 3, this.height / 3 + this.height / 4 + 60, this.width / 3 / 2 - 2, 20, I18n.format("show.report.send"))
                 .setClickListener { send() })
-        registerComponent(Button(this.width / 3 + this.width / 3 / 2 + 2, this.height / 3 + this.height / 4 + 60, this.width / 3 / 2 - 2, 20, "Close")
+        registerComponent(Button(this.width / 3 + this.width / 3 / 2 + 2, this.height / 3 + this.height / 4 + 60, this.width / 3 / 2 - 2, 20, I18n.format("show.report.close"))
                 .setClickListener { this.getStage().displayPrevious() })
     }
 
