d2c443a402ce4628090d6e5fbbec81292f47848c
RedEnergy <forwot163@gmail.com>
2015-12-10 11:28:36 +0400

    Gzip compressor: changed to object class

--- src/main/kotlin/ru/redenergy/report/common/compression/GZIPCompressor.kt
+++ src/main/kotlin/ru/redenergy/report/common/compression/GZIPCompressor.kt
@@ -4,28 +4,29 @@ import java.io.*
 import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
 
-/**
- * Compresses given string to byte array using gzip algorithm
- */
-fun compress(data: String): ByteArray{
-    var stream = ByteArrayOutputStream()
-    var gzip = GZIPOutputStream(stream)
-    with(OutputStreamWriter(gzip, Charsets.UTF_8)) {
-        write(data)
-        close()
+object GZIPCompressor{
+    /**
+     * Compresses given string to byte array using gzip algorithm
+     */
+    fun compress(data: String): ByteArray {
+        var stream = ByteArrayOutputStream()
+        var gzip = GZIPOutputStream(stream)
+        with(OutputStreamWriter(gzip, Charsets.UTF_8)) {
+            write(data)
+            close()
+        }
+        return stream.toByteArray()
     }
-    return stream.toByteArray()
-}
 
-/**
- * Decompress gzip byte array to string
- */
-fun decompress(data: ByteArray): String{
-    var reader = InputStreamReader(GZIPInputStream(ByteArrayInputStream(data)))
-    var builder = StringBuilder()
-    reader.buffered().forEachLine {
-        builder.append(it)
+    /**
+     * Decompress gzip byte array to string
+     */
+    fun decompress(data: ByteArray): String {
+        var reader = InputStreamReader(GZIPInputStream(ByteArrayInputStream(data)))
+        var builder = StringBuilder()
+        reader.buffered().forEachLine {
+            builder.append(it)
+        }
+        return builder.toString()
     }
-    return builder.toString()
 }
-
