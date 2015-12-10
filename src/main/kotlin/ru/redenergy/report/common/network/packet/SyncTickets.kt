08f7d7d415445144f672204c330ca82569265a39
RedEnergy <forwot163@gmail.com>
2015-12-10 11:35:42 +0400

    SyncTickets packet: added gzip compression

--- src/main/kotlin/ru/redenergy/report/common/network/packet/SyncTickets.kt
+++ src/main/kotlin/ru/redenergy/report/common/network/packet/SyncTickets.kt
@@ -8,17 +8,22 @@ import io.netty.channel.ChannelHandlerContext
 import net.minecraft.entity.player.EntityPlayer
 import org.apache.commons.lang3.reflect.FieldUtils
 import ru.redenergy.report.client.QReportClient
+import ru.redenergy.report.common.compression.GZIPCompressor
 import ru.redenergy.report.common.entity.Ticket
 import ru.redenergy.report.common.network.AbstractPacket
 
 class SyncTickets(var tickets: MutableList<Ticket>) : AbstractPacket{
 
     override fun encodeInto(ctx: ChannelHandlerContext, buf: ByteBuf) {
-        ByteBufUtils.writeUTF8String(buf, Gson().toJson(tickets))
+        var json = Gson().toJson(tickets)
+        var bytes = GZIPCompressor.compress(json)
+        buf.writeBytes(bytes)
     }
 
     override fun decodeInto(ctx: ChannelHandlerContext, buf: ByteBuf) {
-        this.tickets = Gson().fromJson(ByteBufUtils.readUTF8String(buf),
+        var bytes = buf.array().copyOfRange(1, buf.array().size) //we need to cut out first byte because it is packet discriminator
+        var json = GZIPCompressor.decompress(bytes)
+        this.tickets = Gson().fromJson(json,
                 FieldUtils.getField(SyncTickets::class.java, "tickets", true).genericType)
     }
 
