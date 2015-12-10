5fcf74c534750d41864f9f21a7d5636d668eae20
RedEnergy <forwot163@gmail.com>
2015-12-10 11:23:48 +0400

    Implemented Gzip compressor

---
+++ src/main/kotlin/ru/redenergy/report/common/compression/GZIPCompressor.kt
@@ -0,0 +1,31 @@
+package ru.redenergy.report.common.compression
+
+import java.io.*
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
+
+/**
+ * Compresses given string to byte array using gzip algorithm
+ */
+fun compress(data: String): ByteArray{
+    var stream = ByteArrayOutputStream()
+    var gzip = GZIPOutputStream(stream)
+    with(OutputStreamWriter(gzip, Charsets.UTF_8)) {
+        write(data)
+        close()
+    }
+    return stream.toByteArray()
+}
+
+/**
+ * Decompress gzip byte array to string
+ */
+fun decompress(data: ByteArray): String{
+    var reader = InputStreamReader(GZIPInputStream(ByteArrayInputStream(data)))
+    var builder = StringBuilder()
+    reader.buffered().forEachLine {
+        builder.append(it)
+    }
+    return builder.toString()
+}
+
