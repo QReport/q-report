ba67a3ee3bfadcfbd4aa6482c50781def7e6ef5f
RedEnergy <forwot163@gmail.com>
2015-12-14 13:38:24 +0400

    Stats: updated docs

--- src/main/kotlin/ru/redenergy/report/common/Stats.kt
+++ src/main/kotlin/ru/redenergy/report/common/Stats.kt
@@ -5,6 +5,6 @@ package ru.redenergy.report.common
  *
  * @param tickets: Contains amount of tickets in each category (TicketReason: amount)
  * @param activeUsers: Contains five of most active users (UserName: amount)
- * @param averageTime: Average response time for a ticket (in milliseconds), if 0
+ * @param averageTime: Average response time for a ticket (in milliseconds), equal to -1 if no responses registered
  */
 data class Stats(val tickets: Map<TicketReason, Int>, val activeUsers: Map<String, Int>, val averageTime: Long)
