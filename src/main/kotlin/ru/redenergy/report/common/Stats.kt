a05ed70e36157c9135a016f4bbaacf74a043e54d
RedEnergy <forwot163@gmail.com>
2015-12-14 12:59:22 +0400

    Stats: MutablesMaps changed to Map

--- src/main/kotlin/ru/redenergy/report/common/Stats.kt
+++ src/main/kotlin/ru/redenergy/report/common/Stats.kt
@@ -7,4 +7,4 @@ package ru.redenergy.report.common
  * @param activeUsers: Contains five of most active users (UserName: amount)
  * @param averageTime: Average response time for a ticket (int minutes)
  */
-data class Stats(val tickets: MutableMap<TicketReason, Int>, val activeUsers: MutableMap<String, Int>, val averageTime: Int)
+data class Stats(val tickets: Map<TicketReason, Int>, val activeUsers: Map<String, Int>, val averageTime: Int)
