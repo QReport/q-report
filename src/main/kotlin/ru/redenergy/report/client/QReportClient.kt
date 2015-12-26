25e2dd4c0332c0583b289f0d1f7e529242ed57ab
RedEnergy <forwot163@gmail.com>
2015-12-26 23:24:04 +0400

    QReportClient: Added field for synced stats and new packet registation

--- src/main/kotlin/ru/redenergy/report/client/QReportClient.kt
+++ src/main/kotlin/ru/redenergy/report/client/QReportClient.kt
@@ -4,13 +4,11 @@ import com.rabbit.gui.GuiFoundation
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.event.FMLInitializationEvent
 import cpw.mods.fml.common.event.FMLPostInitializationEvent
-import net.minecraft.client.settings.KeyBinding
 import org.lwjgl.input.Keyboard
-import ru.redenergy.report.common.entity.Ticket
 import ru.redenergy.report.client.keyboard.KeyboardController
-import ru.redenergy.report.client.ui.ReportShow
 import ru.redenergy.report.client.ui.SupportShow
-import ru.redenergy.report.client.ui.TicketsListShow
+import ru.redenergy.report.common.Stats
+import ru.redenergy.report.common.entity.Ticket
 import ru.redenergy.report.common.network.NetworkHandler
 import ru.redenergy.report.common.network.packet.*
 
@@ -20,25 +18,29 @@ object QReportClient {
     /**
      * Contains tickets which have been received from server
      */
-    public var syncedTickets : MutableList<Ticket> = arrayListOf()
+    public var syncedTickets: MutableList<Ticket> = arrayListOf()
+    /**
+     * Contains statistics which have been received from server
+     */
+    public var syncedStats: Stats = Stats(mapOf(), mapOf(), -1L)
     /**
      * Shows if current player can manage user requests, if set to true 'admin' button will be available
      */
     public var adminAccess = false
 
     @Mod.EventHandler
-    public fun postInit(event: FMLPostInitializationEvent){
+    public fun postInit(event: FMLPostInitializationEvent) {
         registerKeys()
         NetworkHandler.instance.initialise()
         println("QReport Loaded")
     }
 
     @Mod.EventHandler
-    public fun init(event: FMLInitializationEvent){
+    public fun init(event: FMLInitializationEvent) {
         registerPackets()
     }
 
-    private fun registerPackets(){
+    private fun registerPackets() {
         NetworkHandler.instance.apply {
             registerPacket(TicketPacket::class)
             registerPacket(RequestSyncPacket::class)
@@ -46,13 +48,14 @@ object QReportClient {
             registerPacket(AddMessagePacket::class)
             registerPacket(UpdateAdminAccess::class)
             registerPacket(ChangeTicketStatus::class)
+            registerPacket(SyncStatsPackets::class)
         }
     }
 
-    private fun registerKeys(){
+    private fun registerKeys() {
         NetworkHandler.instance.postInitialize()
         var keyboardController = KeyboardController()
-        keyboardController.register(Keyboard.KEY_R, "Support center") { if(Keyboard.isKeyDown(Keyboard.KEY_TAB)) GuiFoundation.display(SupportShow()) }
+        keyboardController.register(Keyboard.KEY_R, "Support center") { if (Keyboard.isKeyDown(Keyboard.KEY_TAB)) GuiFoundation.display(SupportShow()) }
         keyboardController.submit()
     }
 }
