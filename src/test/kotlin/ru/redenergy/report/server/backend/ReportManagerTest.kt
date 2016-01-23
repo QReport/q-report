38e0df939ef83537e836b85ad1f04101f3d041c5
RedEnergy <forwot163@gmail.com>
2016-01-23 22:34:27 +0400

    Rewritten tests

--- src/test/kotlin/ru/redenergy/report/server/backend/ReportManagerTest.kt
+++ src/test/kotlin/ru/redenergy/report/server/backend/ReportManagerTest.kt
@@ -1,12 +1,22 @@
 package ru.redenergy.report.server.backend
 
+import com.j256.ormlite.dao.BaseDaoImpl
+import com.j256.ormlite.dao.Dao
 import com.j256.ormlite.jdbc.JdbcConnectionSource
+import com.j256.ormlite.table.DatabaseTableConfig
 import com.j256.ormlite.table.TableUtils
+import junit.framework.TestSuite
+import org.apache.commons.lang3.reflect.FieldUtils
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import ru.redenergy.report.common.entity.Ticket
 import java.io.File
 import org.junit.Assert.*
+import ru.redenergy.report.common.TicketReason
+import ru.redenergy.report.common.TicketStatus
+import ru.redenergy.report.common.entity.TicketMessage
+import java.util.*
 
 val path = "jdbc:sqlite:${File("").absolutePath}/reports-test.sqlite"
 
@@ -20,13 +30,41 @@ class ReportManagerTest {
     }
 
     @Test
-    fun testAddReport(){
-        reportManager.addTicket(Ticket("MagicPlayer", "Text"))
+    fun testGetActiveUsers(){
+        val tickets = arrayListOf<Ticket>();
+
+        //create a list with 15 tickets per 5 users
+        for((amount, user) in arrayOf("First", "Second", "Third", "Fourth", "Fifth", "Sixth").withIndex()){
+            for(index in 0..amount){
+                tickets.add(Ticket(UUID.randomUUID(), TicketStatus.OPEN, user, TicketReason.OTHER,
+                        arrayListOf(TicketMessage(user, "Text"))))
+            }
+        }
+
+        //get five most active users
+        val users = reportManager.getActiveUsers(tickets, 5)
+
+        //check if user with lowest tickets amount not included
+        assertFalse(users.containsKey("First"))
+
+        //check if tickets amount calculated correctly
+        assertTrue(users["Second"] == 2)
+        assertTrue(users["Third"] == 3)
+        assertTrue(users["Fourth"] == 4)
+        assertTrue(users["Fifth"] == 5)
+        assertTrue(users["Sixth"] == 6)
     }
 
     @Test
-    fun testGetReports(){
-        reportManager.getTickets().forEach { println(it) }
+    fun testAddReport(){
+        //create new ticket and insert it into database
+        val ticket = reportManager.newTicket("Ticket text", TicketReason.BUG, "Sender")
+        //check if tickets has been inserted
+        assertNotNull(reportManager.ticketDao.queryForId(ticket.uid))
     }
 
+    @After
+    fun clean(){
+        TableUtils.clearTable(reportManager.connectionSource, (reportManager.ticketDao as BaseDaoImpl<*, *>).tableConfig)
+    }
 }
