8a2c5694d86ed51b47e8355d5f135c47f885fa14
RedEnergy <forwot163@gmail.com>
2016-03-23 21:44:32 +0400

    Button in inventory now have different position if opened in creativ

--- src/main/kotlin/ru/redenergy/report/client/handler/UIOpenHandler.kt
+++ src/main/kotlin/ru/redenergy/report/client/handler/UIOpenHandler.kt
@@ -3,6 +3,7 @@ package ru.redenergy.report.client.handler
 import com.rabbit.gui.GuiFoundation
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
 import net.minecraft.client.gui.GuiButton
+import net.minecraft.client.gui.inventory.GuiContainerCreative
 import net.minecraft.client.gui.inventory.GuiInventory
 import net.minecraft.util.EnumChatFormatting
 import net.minecraftforge.client.event.GuiScreenEvent
@@ -13,9 +14,12 @@ class UIOpenHandler {
 
     @SubscribeEvent
     fun onUIOpen(event: GuiScreenEvent.InitGuiEvent.Post){
-        if(event.gui is GuiInventory){
-            event.buttonList.add(GuiButton(1024, event.gui.width / 2 + 90, event.gui.height / 2 - 80, 20, 20, "${EnumChatFormatting.BOLD}?"))
-        }
+        val gameInventory = event.gui is GuiInventory
+        val creativeInventory = event.gui is GuiContainerCreative
+        val btnX = event.gui.width / 2 + 90 + (if (creativeInventory) 10 else 0)
+        val btnY = event.gui.height / 2 - 80 - (if (creativeInventory) 10 else 0)
+        if(gameInventory || creativeInventory)
+            event.buttonList.add(GuiButton(1024, btnX, btnY, 20, 20, "${EnumChatFormatting.BOLD}?"))
     }
 
     @SubscribeEvent
