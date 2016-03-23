d2512308f8bd7ec69f11d4fc84aee10760adb1c9
RedEnergy <forwot163@gmail.com>
2016-03-23 20:33:57 +0400

    Slightry updated inventory button position, question mark is now bold

--- src/main/kotlin/ru/redenergy/report/client/handler/UIOpenHandler.kt
+++ src/main/kotlin/ru/redenergy/report/client/handler/UIOpenHandler.kt
@@ -4,6 +4,7 @@ import com.rabbit.gui.GuiFoundation
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
 import net.minecraft.client.gui.GuiButton
 import net.minecraft.client.gui.inventory.GuiInventory
+import net.minecraft.util.EnumChatFormatting
 import net.minecraftforge.client.event.GuiScreenEvent
 import ru.redenergy.report.client.ui.SupportShow
 
@@ -13,7 +14,7 @@ class UIOpenHandler {
     @SubscribeEvent
     fun onUIOpen(event: GuiScreenEvent.InitGuiEvent.Post){
         if(event.gui is GuiInventory){
-            event.buttonList.add(GuiButton(1024, event.gui.width / 2 + 95, event.gui.height / 2 - 80, 20, 20, "?"))
+            event.buttonList.add(GuiButton(1024, event.gui.width / 2 + 90, event.gui.height / 2 - 80, 20, 20, "${EnumChatFormatting.BOLD}?"))
         }
     }
 
